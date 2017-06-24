4413ba4b6ee54ab2031ccba8168b52ce9dd6a033
Triodin <ufolkin1@gmail.com>
2017-06-24 04:31:44 +0300

    Doxygen configured

--- include/JsonFormer.h
+++ include/JsonFormer.h
@@ -1,9 +1,15 @@
+/** @file
+ *  @brief JSON former object, to form server replies in JSON format
+ */
+
 //
 // Created by George Kobelev on 6/22/17.
 //
 
 #ifndef LAB8_JSONFORMER_H
 #define LAB8_JSONFORMER_H
+
+
 
 #include <string>
 #include "json.hpp"
@@ -12,19 +18,134 @@
 
 using nlohmann::json;
 
+/**
+ * @class JsonFormer
+ * @brief Class for forming JSON-formatted output string for server reply to client request
+ */
 class JsonFormer {
 private:
+
+    /**
+     * @brief Read whole file to std::string and return it
+     * @param filename Name of file to read
+     * @return std::string of file content
+     */
     std::string readFile(std::string filename);
+
+    /**
+     * @brief Check size of file in bytes
+     * @param filename Name of file to check size
+     * @return size_t size of file in bytes
+     */
     size_t filesize(std::string filename);
 
+    /**
+     * @brief Forming JSON string of server info and write to output
+     *
+       ```json
+        {
+            "title": "%SERVER_NAME%",
+            "developer": "%YOUR_NAME_SURNAME%",
+            "time": "%SERVER_TIME%"
+        }
+        ```
+     */
     void formServerInfo();
+
+    /**
+     * @brief Forming JSON string of favorites plants and write to output
+     *
+     *  ```json
+        [
+            {
+                "id": 1,
+                "name": "Asparagus",
+                "type": "Vegetable",
+                "companions": ["Tomato", "Aster", "Dill"],
+                "foes": ["Onion", "Garlic", "Potato"],
+                "description": "Description..."
+            },
+
+            {
+                "id": 2,
+                "name": "Anise",
+                "type": "Herb",
+                "companions": ["Coriander", "Cilantro"],
+                "foes": [],
+                "description": "Description..."
+            },
+
+            ...
+
+            {
+                "id": 50,
+                "name": "Watercress",
+                "type": "Vegetable",
+                "companions": ["Onion", "Chive", "Peppermint", "Spearmint"],
+                "foes": [],
+                "description": "Description..."
+            }
+       ]
+       ```
+     */
     void formFavorites();
+
+    /**
+     * @brief Forming JSON string of data about file <code>data.txt</code> (num of letters, digits) and write to output
+     *
+     *  ```json
+        {
+            "letters": 1234,
+            "digits": 4321
+        }
+        ```
+     */
     void formFileData();
+
+    /**
+     * @brief Forming JSON string about file <code>data.txt</code> (size, name, content) and write to output
+     *
+     *  ```json
+        {
+            "filename": "%FILE_NAME%",
+            "size": 1234,
+            "content": "%FILE_CONTENT"
+        }
+        ```
+     */
     void formFile();
+
+    /**
+     * @brief Forming JSON string of favorite plants with needed <code>{key}={value}</code> from req and write to output
+     * @param[in] req Processed request from client for key, value
+     *
+     *  ```json
+        http://localhost:8080/favorites?name=SummerSavory
+
+        {
+            "id": 45,
+            "name": "SummerSavory",
+            "type": "Herb",
+            "companions": ["Green Bean", "Onion"],
+            "foes": [],
+            "description": "Description..."
+        }
+        ```
+     */
     void formFavoritesByKey(ReqProcessor req);
 
 public:
+
+    /**
+     * @brief Output string that contains JSON formatted data or <code>404 Not Found</code> status/ for server  reply.
+     */
     std::string output;
+
+    /**
+     * @brief Method to form output JSON formatted string from processed client req for server reply
+     * @param[in] req Processed request from client for request type, key, value
+     * @return output
+     */
     std::string formJsonStr(ReqProcessor req);
 
 };
--- include/ReqProcessor.h
+++ include/ReqProcessor.h
@@ -1,3 +1,8 @@
+/**
+ * @file
+ * @brief Contains ReqProcessor class, ReqType enumeration and CrudeReq structure
+ */
+
 //
 // Created by George Kobelev on 6/22/17.
 //
@@ -10,39 +15,77 @@
 #include <boost/algorithm/string.hpp>
 #include <vector>
 
+///@brief The enumeration of client request types.
 enum ReqType{
-    REQ_DEFAULT,         // "/"
-    REQ_FAVORITES_ALL,   // "/favorites"
-    REQ_FILE,            // "/file"
-    REQ_FILE_DATA,       // "/file/data"
-    REQ_FAVORITES_BY_ID, // "/favorites/{id}"
-    REQ_FAVORITES_BY_KEY,// "/favorites/{key}={value}"
-    REQ_BAD
+    REQ_DEFAULT,         ///< <code>/</code> request JSON with server information.
+    REQ_FAVORITES_ALL,   ///< <code>/favorites</code> request JSON-list of objects, indicating the list of favorite plants.
+    REQ_FILE,            ///< <code>/file</code> request the contents, filename and size of the file <code>data.txt</code>.
+    REQ_FILE_DATA,       ///< <code>/file/data</code> request JSON object with information about the number of all digits and letters in a file (file <code>data.txt</code>).
+    REQ_FAVORITES_BY_ID, ///< <code>/favorites/{id}</code> request JSON object from the list <code>/</code> favorites by identifier with numeric value <code>{id}</code>.
+    REQ_FAVORITES_BY_KEY,///< <code>/favorites?{key}={value}</code> request JSON-sublist of objects from the list <code>/</code> favorites in which the <code>{key}</code> field has a value of <code>{value}</code>.
+    REQ_BAD              ///< Bad request: <code>404 Not Found</code>.
 };
 
+/**
+ * @struct CrudeReq
+ * @brief Structure that contains sliced crude request (path, query).
+ */
 typedef struct CrudeReq{
-    std::string path; // "/blah-blah/blah"
-    std::string query;// "?{key}={value}"
+    std::string path; ///< <code>/blah-blah/blah</code>.
+    std::string query;///< <code>?{key}={value}</code>.
 }CrudeReq;
 
+/**
+ * @class ReqProcessor
+ * @brief Class for processing client request to identify request type and parse path and query.
+ */
 class ReqProcessor {
 private:
     CrudeReq crudeReq;
 
-    std::string uri;
+    std::string uri;///< crude non-sliced client uri.
+
+    /**
+     * @brief Slice uri for CrudeReq, check if is right uri and parse.
+     */
     void processCrude();
 
+    /**
+     * @brief Split std::string by delimiter delim to std::vector<std::string>.
+     * @param[in] s String to split.
+     * @param[in] delim Delimiter on which to split string.
+     * @return vector of s substring.
+     */
     std::vector<std::string> split(std::string s, char delim);
+
+    /**
+     * @brief Check if s string is number.
+     * @param[in] s String to check.
+     * @return true if s is number and false if not.
+     */
     bool is_number(const std::string& s);
+
+    /**
+     * @brief Check if path is right, identify request type and parse.
+     */
     void checkPath();
+
+    /**
+     * @brief Check if query is right, identify request type and parse.
+     */
     void checkQuery();
 public:
     ReqProcessor();
     ReqProcessor(evhttp_request *request);
-    ReqType type = REQ_BAD;
-    int id = 0;
-    std::string key;
-    std::string value;
+    ReqType type = REQ_BAD;///< client request type (REQ_BAD by default).
+    int id = 0;///< <code>{id}</code> of JSON object from <code>favorites.json</code>.
+    std::string key;///< <code>{key}</code> on which JsonFromer formFavoritesByKey.
+    std::string value;///< <code>{value}</code> to find and reply in JsonFromer formFavoritesByKey.
+
+    /**
+     * @brief Set uri string and process request.
+     * @param request Request to process.
+     */
     void setUri(evhttp_request *request);
 };
 
